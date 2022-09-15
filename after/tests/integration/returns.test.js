a432f01180c74512a063e1409bc304f2ab69aa04
Usama Tester <usamanjc@gmail.com>
2022-09-15 19:43:08 +0500

    Added New Test File for returns,js

---
+++ after/tests/integration/returns.test.js
@@ -0,0 +1,40 @@
+const { Rental } = require("../../models/rental");
+const mongoose = require("mongoose");
+
+describe("/api/returns", () => {
+  let server;
+  let customerId;
+  let movieId;
+  let rental;
+
+  beforeEach(() => {
+    server = require("../../index");
+
+    customerId = mongoose.Types.ObjectId();
+    movieId = mongoose.Types.ObjectId();
+
+    rental = new Rental({
+      customer: {
+        _id: customerId,
+        name: "Shaikh Usama Bin Naeem",
+        phone: "0324xxxxxxx",
+      },
+      movie: {
+        _id: movieId,
+        title: "Taare Zameen Par",
+        dailyRentalRate: 2,
+      },
+    });
+    rental.save();
+  });
+  afterEach(async () => {
+    server.close();
+    await Rental.remove({});
+  });
+
+  test('Should Work!', async () => {
+    const result = await Rental.findById(rental._id);
+
+    expect(result).not.toBeNull();
+  });
+});
