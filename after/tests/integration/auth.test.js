66cd66ddfb074f661fecdd65fc050982c614781d
Usama Tester <usamanjc@gmail.com>
2022-09-15 15:38:08 +0500

    First Integration Test - Return 401 if no token is provided

--- after/tests/integration/auth.test.js
+++ after/tests/integration/auth.test.js
@@ -1,33 +1,62 @@
 const request = require("supertest");
-const { User } = require("../../models/user");
-let server;
+const { User } = require('../../models/user');
 
-describe("Auth Middleware", () => {
-  beforeEach(() => {
-    server = require("../../index");
-  });
-  afterEach(async () => {
-    server.close();
-  });
+describe("auth middleware", () => {
+  beforeEach(() => { server = require("../../index"); });
+  afterEach(async () => { server.close(); });
 
   let token;
 
-  const exec = async() => {
-    return await request(server)
-      .post("/api/genres")
-      .set("x-auth-token", token)
-      .send({ name: "Shaikh Usama Bin Naeem" });
+  const exec = () => {
+    return request(server)
+      .post('/api/genres')
+      .set('x-auth-token', token)
+      .send({ name: 'Genre1' });
   };
 
   beforeEach(() => {
     token = new User().generateAuthToken();
   });
 
-  test("Returns 401 if no token is provided", async () => {
-    token = "";
+  test("Return 401 if no token is provided", async () => {
+    token = '';
 
     const res = await exec();
-
+    
     expect(res.status).toBe(401);
   });
 });
+
+// const request = require("supertest");
+// const { User } = require("../../models/user");
+// let server;
+
+// describe("Auth Middleware", () => {
+//   beforeEach(() => {
+//     server = require("../../index");
+//   });
+//   afterEach(async () => {
+//     server.close();
+//   });
+
+//   let token;
+
+//   const exec = async() => {
+//     return await request(server)
+//       .post("/api/genres")
+//       .set("x-auth-token", token)
+//       .send({ name: "Shaikh Usama Bin Naeem" });
+//   };
+
+//   beforeEach(() => {
+//     token = new User().generateAuthToken();
+//   });
+
+//   test("Returns 401 if no token is provided", async () => {
+//     token = "";
+
+//     const res = await exec();
+
+//     expect(res.status).toBe(401);
+//   });
+// });
