784ebed1362ba14501661fdea76a78799d3574ae
Usama Tester <usamanjc@gmail.com>
2022-09-15 14:08:40 +0500

    Added 5 new Integration Tests

--- after/tests/integration/genres.test.js
+++ after/tests/integration/genres.test.js
@@ -1,24 +1,67 @@
 const request = require("supertest");
-const {Genre} = require('../../models/genre');
+const { Genre } = require("../../models/genre");
+const { User } = require("../../models/user");
 let server;
 
 describe("/api/genres", () => {
   beforeEach(() => {
     server = require("../../index");
   });
-  afterEach(() => {
+  afterEach(async () => {
     server.close();
+    await Genre.remove({});
   });
   describe("GET /", () => {
     test("Returns all genres", async () => {
       await Genre.collection.insertMany([
-        { name: 'Genre1' },
-        { name: 'Genre2' },
+        { name: "Genre1" },
+        { name: "Genre2" },
       ]);
 
-      const res = await request(server).get('/api/genres');
+      const res = await request(server).get("/api/genres");
       expect(res.status).toBe(200);
       expect(res.body.length).toBe(2);
+      expect(res.body.some((g) => g.name === "Genre1")).toBeTruthy();
+      expect(res.body.some((g) => g.name === "Genre2")).toBeTruthy();
+    });
+  });
+
+  describe("GET /:id", () => {
+    test("Return genre if valid id is passed", async () => {
+      const genre = new Genre({ name: "Genre1" });
+      await genre.save();
+
+      const res = await request(server).get("/api/genres/" + genre._id);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("name", genre.name);
+    });
+
+    test("Return 404 if invalid id is passed", async () => {
+      const res = await request(server).get("/api/genres/1");
+
+      expect(res.status).toBe(404);
+    });
+  });
+
+  describe("POST /", () => {
+    test("Return 401 Error if client is not logged in", async () => {
+      const res = await request(server)
+        .post("/api/genres")
+        .send({ name: "Genre1" });
+
+      expect(res.status).toBe(401);
+    });
+
+    test("Return 400 Error if genre is less than 5 characters", async () => {
+      const token = new User().generateAuthToken();
+
+      const res = await request(server)
+        .post("/api/genres")
+        .set('x-auth-token', token)
+        .send({ name: "1234" });
+
+      expect(res.status).toBe(400);
     });
   });
 });
