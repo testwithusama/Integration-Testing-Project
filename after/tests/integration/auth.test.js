c8d77cf53de7c321d3bd69421850e83bb01e7f95
Usama Tester <usamanjc@gmail.com>
2022-09-15 19:05:05 +0500

    Deleted Outdated tests

--- after/tests/integration/auth.test.js
+++ after/tests/integration/auth.test.js
@@ -45,37 +45,3 @@ describe("auth middleware", () => {
     expect(res.status).toBe(200);
   });
 });
-
-// const request = require("supertest");
-// const { User } = require("../../models/user");
-// let server;
-
-// describe("Auth Middleware", () => {
-//   beforeEach(() => {
-//     server = require("../../index");
-//   });
-//   afterEach(async () => {
-//     server.close();
-//   });
-
-//   let token;
-
-//   const exec = async() => {
-//     return await request(server)
-//       .post("/api/genres")
-//       .set("x-auth-token", token)
-//       .send({ name: "Shaikh Usama Bin Naeem" });
-//   };
-
-//   beforeEach(() => {
-//     token = new User().generateAuthToken();
-//   });
-
-//   test("Returns 401 if no token is provided", async () => {
-//     token = "";
-
-//     const res = await exec();
-
-//     expect(res.status).toBe(401);
-//   });
-// });
