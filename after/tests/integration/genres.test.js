a42590c93feb553b4096e4816000b705e13b47cf
Usama Tester <usamanjc@gmail.com>
2022-09-15 13:14:45 +0500

    Changes in the genres integration test file

--- after/tests/integration/genres.test.js
+++ after/tests/integration/genres.test.js
@@ -1,107 +1,24 @@
 const request = require("supertest");
-const { Genre } = require("../../models/genre");
-const { User } = require("../../models/user");
-
+const {Genre} = require('../../models/genre');
 let server;
 
-describe("api/genres", () => {
+describe("/api/genres", () => {
   beforeEach(() => {
     server = require("../../index");
   });
-  afterEach(async () => {
+  afterEach(() => {
     server.close();
-    await Genre.remove({});
   });
-
   describe("GET /", () => {
-    test("Returns all the genres", async () => {
+    test("Returns all genres", async () => {
       await Genre.collection.insertMany([
-        { name: "Shaikh Usama Bin Naeem" },
-        { name: "Abdul Hanan Asif" },
+        { name: 'Genre1' },
+        { name: 'Genre2' },
       ]);
 
-      const res = await request(server).get("/api/genres");
-
+      const res = await request(server).get('/api/genres');
       expect(res.status).toBe(200);
       expect(res.body.length).toBe(2);
-      expect(
-        res.body.some((g) => g.name === "Shaikh Usama Bin Naeem")
-      ).toBeTruthy();
-      expect(res.body.some((g) => g.name === "Abdul Hanan Asif")).toBeTruthy();
-    });
-  });
-
-  describe("GET /:id ", () => {
-    test("Returns a genre if valid id is passed", async () => {
-      const genre = new Genre({ name: "Shaikh Usama Bin Naeem" });
-      await genre.save();
-
-      const res = await request(server).get("/api/genres/" + genre._id);
-
-      expect(res.status).toBe(200);
-      expect(res.body).toHaveProperty("name", genre.name);
-    });
-
-    test("Returns 404 if invalid id is passed", async () => {
-      const res = await request(server).get("/api/genres/1");
-
-      expect(res.status).toBe(404);
-    });
-  });
-
-  describe("POST /", () => {
-    let token;
-    let name;
-
-    const exec = async() => {
-      return await request(server)
-          .post("/api/genres")
-        .set("x-auth-token", token)
-        .send({ name });
-    };
-
-    beforeEach(() => {
-      token = new User().generateAuthToken();
-      name = "Shaikh Usama Bin Naeem";
-    });
-
-    test("Returns 401 if client is not logged in", async () => {
-      token = "";
-
-      const res = await exec();
-
-      expect(res.status).toBe(401);
-    });
-
-    test("Returns 400 if genre less than 5 characters", async () => {
-      name = "1234";
-
-      const res = await exec();
-
-      expect(res.status).toBe(400);
-    });
-
-    test("Returns 400 if genre is more than 50 characters", async () => {
-      name = new Array(52).join("a");
-
-      const res = await exec();
-
-      expect(res.status).toBe(400);
-    });
-
-    test("Save the genre if it is valid", async () => {
-      await exec();
-
-      const genre = await Genre.find({ name: "Shaikh Usama Bin Naeem" });
-
-      expect(genre).not.toBeNull();
-    });
-
-    test("Return the genre if it is valid", async () => {
-      const res = await exec();
-
-      expect(res.body).toHaveProperty("_id");
-      expect(res.body).toHaveProperty("name", "Shaikh Usama Bin Naeem");
     });
   });
 });
