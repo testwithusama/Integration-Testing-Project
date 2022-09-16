99552b25fad9821c5355428984242cc710d6b7c0
Usama Tester <usamanjc@gmail.com>
2022-09-16 12:49:31 +0500

    Updated the endpoint according to new test

--- after/routes/returns.js
+++ after/routes/returns.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-    res.status(401).send('Unauthorized');
-})
+router.post("/", async (req, res) => {
+  if (!req.body.customerId) return res.status(400).send("Customer Id not provided");
+
+  res.status(401).send("Unauthorized");
+});
 
 module.exports = router;
