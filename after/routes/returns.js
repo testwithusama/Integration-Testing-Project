a7039a49df0ed4bbbc1499fb99cec155205e16c1
Usama Tester <usamanjc@gmail.com>
2022-09-16 14:23:25 +0500

    Added a new end point according to the test

--- after/routes/returns.js
+++ after/routes/returns.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const { Rental } = require('../models/rental');
 
 router.post("/", async (req, res) => {
   if (!req.body.customerId) return res.status(400).send("Customer Id not provided");
 
   if (!req.body.movieId) return res.status(400).send("Movie Id is not provided");
+
+  const rental = await Rental.findOne({
+    'customer._id': req.body.customerId,
+    'movie._id': req.body.movieId,
+  });
+
+  if (!rental) return res.status(404).send("Rental not found.")
 
   res.status(401).send("Unauthorized");
 });
