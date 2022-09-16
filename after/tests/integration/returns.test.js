d976e8f40c158b734a4a2ac842515b4a6c3fbfe0
Usama Tester <usamanjc@gmail.com>
2022-09-16 10:57:58 +0500

    Updated First TDD Test - To check if it is working

--- after/tests/integration/returns.test.js
+++ after/tests/integration/returns.test.js
@@ -1,38 +1,41 @@
-const { Rental } = require("../../models/rental");
-const mongoose = require("mongoose");
+const { Rental } = require('../../models/rental');
+const mongoose = require('mongoose');
 
-describe("/api/returns", () => {
+describe('/api/returns', () => {
   let server;
   let customerId;
   let movieId;
   let rental;
 
-  beforeEach(() => {
-    server = require("../../index");
-
+  beforeEach(async () => { 
+    server = require('../../index');
+    
     customerId = mongoose.Types.ObjectId();
     movieId = mongoose.Types.ObjectId();
 
-    rental = new Rental({
+    rental = new Rental ({
       customer: {
         _id: customerId,
-        name: "Shaikh Usama Bin Naeem",
-        phone: "0324xxxxxxx",
+        name: 'Shaikh Usama Bin Naeem',
+        phone: '12345'
       },
+
       movie: {
         _id: movieId,
-        title: "Taare Zameen Par",
-        dailyRentalRate: 2,
-      },
+        title: 'Testing',
+        dailyRentalRate: 2
+      }
+
     });
-    rental.save();
+    await rental.save();
   });
+
   afterEach(async () => {
     server.close();
     await Rental.remove({});
   });
 
-  test("Should Work!", async () => {
+  test('Should Work!', async () => {
     const result = await Rental.findById(rental._id);
 
     expect(result).not.toBeNull();
